897778bfa235bc003a0adb705a611aaf6125602b
SeredaAI <sereda-2009@mail.ru>
2024-01-02 14:28:57 +0300

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,42 @@
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:html/parser.dart';
+import 'package:http/http.dart' as http;
+import 'package:footer/footer.dart';
+
+class AppPlatform {
+  static const Map<String, CustomPlatform> _platfromMap = {
+    'linux': CustomPlatform.linux,
+    'macos': CustomPlatform.macos,
+    'windows': CustomPlatform.windows,
+    'android': CustomPlatform.android,
+    'ios': CustomPlatform.ios,
+    'fuchsia': CustomPlatform.fuchsia,
+  };
+
+  static CustomPlatform? _getPlatform() {
+    if (kIsWeb) {
+      return CustomPlatform.web;
+    }
+
+    return _platfromMap[Platform.operatingSystem] ?? CustomPlatform.undefined;
+  }
+
+  static CustomPlatform? get platform => _getPlatform();
+}
+
+enum CustomPlatform {
+  linux,
+  macos,
+  windows,
+  ios,
+  android,
+  fuchsia,
+  web,
+  undefined,
+}
 
 void main() {
   runApp(const MyApp());
@@ -13,21 +51,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
@@ -39,15 +62,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -55,71 +69,117 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  late TextEditingController _controller;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  String _bodyText = '';
+  String _pageTitle = '';
+  String _corsHeader = '';
+
+  Future<(String, String, String)> fetchData() async {
+    var customHeaders = {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Credentials": "true",
+      "Access-Control-Allow-Headers":
+          "Origin,Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,locale",
+      "Access-Control-Allow-Methods": "POST, OPTIONS"
+    };
+
+    var result = await http.get(Uri.parse(userUrl), headers: customHeaders);
+
+    String cors = 'CORS Header: None';
+    result.headers.forEach((key, value) {
+      if (key == 'access-control-allow-origin') {
+        cors = value;
+      }
     });
+
+    return (
+      _bodyText = result.body,
+      _pageTitle = parse(result.body).querySelector('h1')!.text,
+      _corsHeader = cors
+    );
   }
 
   @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+  void initState() {
+    super.initState();
+    _controller = TextEditingController();
+    _controller.text = 'https://flutter.dev';
   }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  String userUrl = 'https://flutter.dev';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: SafeArea(
+            child: FutureBuilder<(String, String, String)>(
+                future: fetchData(), // async work
+                builder: (_, snapshot) {
+                  switch (snapshot.connectionState) {
+                    case ConnectionState.waiting:
+                      return const Center(child: Text('Loading....'));
+                    default:
+                      if (snapshot.hasError) {
+                        return Center(child: Text('Error: ${snapshot.error}'));
+                      } else {
+                        return Column(
+                          children: [
+                            Text(_pageTitle),
+                            Text(
+                              _corsHeader,
+                              style: const TextStyle(
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.w600,
+                                  color: Colors.redAccent),
+                            ),
+                            Expanded(flex: 3, child: Text(_bodyText)),
+                            Expanded(
+                              child: TextField(
+                                controller: _controller,
+                                decoration: InputDecoration(
+                                  labelText: 'Введите URL',
+                                  suffixIcon: ElevatedButton(
+                                    child: const Text('LOAD'),
+                                    onPressed: () async {
+                                      userUrl = _controller.value.text;
+                                      // await fetchData(userUrl);
+                                      setState(() {});
+                                    },
+                                  ),
+                                ),
+                                onSubmitted: (value) async {
+                                  userUrl = value;
+                                  // await fetchData(userUrl);
+                                  setState(() {});
+                                },
+                              ),
+                            ),
+                            Footer(
+                                child: platfromText(), //The child Widget is mandatory takes any Customisable Widget for the footer
+                                backgroundColor: Colors.grey
+                                    .shade200, // defines the background Colors of the Footer with default Colors.grey.shade200
+                                padding: const EdgeInsets.all(
+                                    1.0), // Takes EdgeInsetsGeometry with default being EdgeInsets.all(5.0)
+                                alignment: Alignment
+                                    .bottomCenter //this is of type Aligment with default being Alignment.bottomCenter
+                                ),
+                          ],
+                        );
+                      }
+                  }
+                })));
+  }
+}
+
+Text platfromText() {
+  String platfromText = AppPlatform.platform.toString().toUpperCase().replaceAll('CUSTOMPLATFORM.', '');
+
+  return Text('APPLICTION RUNNING ON $platfromText', style: TextStyle(fontWeight: FontWeight.bold));
 }
